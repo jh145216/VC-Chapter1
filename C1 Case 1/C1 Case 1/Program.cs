9f3650c70451801bd9f40a4f14a607aba3d7de13
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-22 14:10:08 -0600

    update case 1

---
+++ C1 Case 1/C1 Case 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1_Case_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*The stars shine in Greenville*");
+            Console.WriteLine("*******************************");
+        }
+    }
+}
