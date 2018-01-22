2122c7b50f9849e608b32db667410f6e73b36dff
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-22 14:10:32 -0600

    update Vine Glass

---
+++ VineGlass/VineGlass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VineGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("XXXXXXXXXXXXX");
+            System.Console.WriteLine(" X         X");
+            System.Console.WriteLine("  X       X");
+            System.Console.WriteLine("   XXXXXXX");
+            System.Console.WriteLine("      X");
+            System.Console.WriteLine("      X");           
+            System.Console.WriteLine("    XXXXX");    
+
+            System.Console.WriteLine("H      H");
+            System.Console.WriteLine("H      H");
+            System.Console.WriteLine("H      H");
+            System.Console.WriteLine("HHHHHHHH");
+            System.Console.WriteLine("H      H");
+            System.Console.WriteLine("H      H");    
+            System.Console.WriteLine("H      H");  
+        }
+    }
+}
