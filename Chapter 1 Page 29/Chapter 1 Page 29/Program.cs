cf63343599c0e6f210791796e39a387305ad1b6a
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-22 14:09:42 -0600

    update Chapter 1

---
+++ Chapter 1 Page 29/Chapter 1 Page 29/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Console;
+
+namespace Chapter_1_Page_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Line one");
+            System.Console.WriteLine("Line two");
+            System.Console.WriteLine("Line three");
+        }
+    }
+}
