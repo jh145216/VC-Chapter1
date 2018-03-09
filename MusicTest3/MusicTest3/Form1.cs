7e2b6f9c273dd89266dca0c218e858a37fc561cd
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-03-09 14:49:56 -0600

    update MT3

---
+++ MusicTest3/MusicTest3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MusicTest3
+{
+    public partial class Form1 : Form
+    {
+        //System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            //player.SoundLocation = "myMusicWav.wav";
+            player.URL = "myMusicmp3.mp3";
+            player.controls.stop();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+    }
+}
